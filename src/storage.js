a4c49d1edcdcb76a50d3fa4e1714a824cc1c4452
Yoav Aner <yoav@gingerlime.com>
2021-01-08 14:35:24 +0000

    improve storage
    * before we were loading data from localStorage and storing it in
      `this.storage`
    * if another JS code uses the same localStorage / namespace and stores
      something there we can accidentally overwrite it when we set the data
      from `this.storage` into localStorage
    * instead we always load the data from localStorage for `get`
      and before we set anything to make sure we're always accessing the
      most accurate data in localStorage

--- src/storage.js
+++ src/storage.js
@@ -6,20 +6,20 @@ class Storage {
       const check = "localstorage_check"
       localStorage.setItem(check, check)
       localStorage.removeItem(check)
-      this.storage = JSON.parse(localStorage.getItem(this.namespace) || "{}")
     } catch {
       throw new Error("localStorage is not available")
     }
   }
 
   set(key, value) {
-    this.storage[key] = value
-    localStorage.setItem(this.namespace, JSON.stringify(this.storage))
+    let storage = JSON.parse(localStorage.getItem(this.namespace) || "{}")
+    storage[key] = value
+    localStorage.setItem(this.namespace, JSON.stringify(storage))
     return value
   }
 
   get(key) {
-    return this.storage[key]
+    return JSON.parse(localStorage.getItem(this.namespace) || "{}")[key]
   }
 }
 
