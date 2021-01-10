b9aee1b7aec0b9f79067fb442027793414d95172
JoKer <joker777vs.666@gmail.com>
2021-01-10 14:31:22 +0100

    another test

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: "alephbet.min.js",
     path: path.resolve(__dirname, "dist"),
     library: "AlephBet",
-    libraryTarget: "umd",
+    libraryTarget: "assign",
     globalObject: "window"
   },
   watchOptions: {
@@ -29,12 +29,7 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
+        use: ["babel-loader"]
       }
     ]
   }
