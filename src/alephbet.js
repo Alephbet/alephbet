9846af8a96bfab7da85bf95eccdc83c31fc1c616
Johannes Köhler <joker777vs.666@gmail.com>
2021-03-02 11:00:29 +0100

    Adds back LamedAdapter (#50)

--- src/alephbet.js
+++ src/alephbet.js
@@ -6,6 +6,7 @@ export {Goal, Experiment}
 export {default as utils} from "./utils"
 export {default as options} from "./options"
 export {default as GimelAdapter} from "./adapters/gimel_adapter"
+export {default as LamedAdapter} from "./adapters/lamed_adapter"
 export {default as AlephbetAdapter} from "./adapters/alephbet_adapter"
 export {default as LocalStorageAdapter} from "./adapters/local_storage_adapter"
 export {
